a1bf60855e766539467467787ca11bfbe64595c0
ch3sda <officialchesdamc@gmail.com>
2024-08-26 07:08:24 +0700

    part mazer deashboard

--- frontend/src/components/mazer/widget/PendingTotals.vue
+++
@@ -1,6 +0,0 @@
-<template>
-    <a href="#" class="md:w-full md:h-full block sm:p-4 p-4 bg-white border border-gray-300 rounded-lg shadow hover:bg-gray-100 dark:bg-primary-blue dark:border-gray-700 dark:hover:bg-gray-700">
-        <h5 class="mb-2 text-lg font-normal md:text-2xl tracking-tight text-primary-blue dark:text-white">Pending Totals</h5> 
-        <p class="font-semibold md:font-bold m-6 md:m-10 text-center text-6xl text-my-yellow dark:text-my-yellow">01</p>
-    </a>
-</template>
--- frontend/src/components/mazer/widget/RequestedTotals.vue
+++
@@ -1,6 +0,0 @@
-<template>
-        <a href="#" class="md:w-full md:h-full block sm:p-4 p-4 bg-primary-blue border border-gray-200 rounded-lg shadow hover:bg-gray-600 dark:bg-primary-blue dark:border-gray-700 dark:hover:bg-gray-700">
-            <h5 class="mb-2 text-lg font-normal md:text-2xl tracking-tight text-white dark:text-white">Requested Totals</h5> 
-            <p class="font-semibold md:font-bold m-6 md:m-10 text-center text-6xl text-white dark:text-white">08</p>
-        </a>
-</template>
